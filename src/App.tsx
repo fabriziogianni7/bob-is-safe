4dc996d807c3858a96aae7143f76efad7150841d
lim0n3 <simonestaffa96@gmail.com>
2023-05-12 18:17:05 +0100

    Enable module tx button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useCallback } from 'react'
 import styled from 'styled-components'
 import { Button, Title } from '@gnosis.pm/safe-react-components'
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
+import {ethers} from "ethers";
+import {safeAbi} from "./safe-abi";
 
 const Container = styled.div`
   padding: 1rem;
@@ -17,8 +19,30 @@ const Link = styled.a`
   margin-top: 8px;
 `
 
+const multiSendModuleAddress = "0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526";
+
 const SafeApp = (): React.ReactElement => {
   const { sdk, safe } = useSafeAppsSDK()
+
+  const enableZKModuleTx = useCallback(async () => {
+    try {
+
+      const { safeTxHash } = await sdk.txs.send({
+        txs: [
+          {
+            to: safe.safeAddress,
+            value: '0',
+            data: ethers.Interface.from(safeAbi).encodeFunctionData("enableModule", [multiSendModuleAddress]),
+          },
+        ],
+      })
+      console.log({ safeTxHash })
+      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
+      console.log({ safeTx })
+    } catch (e) {
+      console.error(e)
+    }
+  }, [safe, sdk])
 
   const submitTx = useCallback(async () => {
     try {
@@ -39,9 +63,14 @@ const SafeApp = (): React.ReactElement => {
     }
   }, [safe, sdk])
 
+
   return (
     <Container>
       <Title size="md">Safe: {safe.safeAddress}</Title>
+
+      <Button size="lg" color="primary" onClick={enableZKModuleTx}>
+        Click to enable ZK module
+      </Button>
 
       <Button size="lg" color="primary" onClick={submitTx}>
         Click to send a test transaction
