3d83a9d85d7d499a7bfa22be973368047a058832
lim0n3 <simonestaffa96@gmail.com>
2023-05-12 20:07:04 +0100

    Check if module enabled

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Button, Title } from '@gnosis.pm/safe-react-components'
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
 import {ethers} from "ethers";
 import {safeAbi} from "./safe-abi";
+import Safe, {EthersAdapter} from "@safe-global/protocol-kit";
 
 const Container = styled.div`
   padding: 1rem;
@@ -24,15 +25,15 @@ const multiSendModuleAddress = "0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526";
 const SafeApp = (): React.ReactElement => {
   const { sdk, safe } = useSafeAppsSDK()
 
+
   const enableZKModuleTx = useCallback(async () => {
     try {
-
       const { safeTxHash } = await sdk.txs.send({
         txs: [
           {
             to: safe.safeAddress,
             value: '0',
-            data: ethers.Interface.from(safeAbi).encodeFunctionData("enableModule", [multiSendModuleAddress]),
+            data: new ethers.utils.Interface(safeAbi).encodeFunctionData("enableModule", [multiSendModuleAddress]),
           },
         ],
       })
@@ -63,14 +64,38 @@ const SafeApp = (): React.ReactElement => {
     }
   }, [safe, sdk])
 
+  const isModuleEnabledQ = useCallback(async () => {
+    try {
+      const ethAdapter = new EthersAdapter({
+        ethers,
+        signerOrProvider: new ethers.providers.Web3Provider(window.ethereum)
+      })
+      const safeSDK = await Safe.create({
+        ethAdapter,
+        safeAddress: safe.safeAddress
+      })
+      console.log(await safeSDK.isModuleEnabled(multiSendModuleAddress));
+    } catch (e) {
+      console.error(e)
+    }
+  }, [safe, sdk])
+
 
   return (
     <Container>
       <Title size="md">Safe: {safe.safeAddress}</Title>
 
+      <Button size="lg" color="primary" onClick={isModuleEnabledQ}>
+        Check if module is enabled
+      </Button>
+
+      <br/>
+
       <Button size="lg" color="primary" onClick={enableZKModuleTx}>
         Click to enable ZK module
       </Button>
+
+      <br/>
 
       <Button size="lg" color="primary" onClick={submitTx}>
         Click to send a test transaction
