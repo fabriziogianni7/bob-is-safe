027ee1d63d272e05d8d155baedf5ef0ce640eea9
Fabriziogianni7 <fabrizio@keyko.io>
2023-05-13 11:06:30 +0100

    update UI

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -1,6 +1,6 @@
 import Safe, { EthersAdapter } from '@safe-global/protocol-kit'
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
-import { Button, Form, Input, Select } from 'antd'
+import { Button, Form, Input, Select, Card, Alert, Space, Spin, Result } from 'antd'
 import { ethers } from 'ethers'
 import React, { useCallback, useEffect, useState } from 'react'
 import safeAbi from '../contracts-abi/safe-abi.json'
@@ -28,18 +28,39 @@ const receiverZkBobAddress = '38aywFhcqbZmncHA8WM1UwKPgrVnqwsViwRWxbjNGBKtQxwb5Y
 const TransactionForm: React.FC = () => {
   const [form] = Form.useForm()
   const { sdk, safe } = useSafeAppsSDK()
-  const [isModuleLoading, setIsModuleLoading] = useState(false)
-  const [isModuleEnabled, setIsModuleEnabled] = useState(false)
 
-  const onFinish = (values: any) => {
-    console.log(values)
-  }
+  const [status, setStatus] = useState<"initial" | "txPending" | "txSuccess">("txSuccess")
+  const [zkBobAddress, setZkBobAddress] = useState<string>("")
+  const [tokenAddress, setTokenAddress] = useState<string>("")
+  const [amount, setAmount] = useState<number>(0)
+  const [isModuleEnabled, setIsModuleEnabled] = useState(false)
+  const [moduleContract, setModuleContract] = useState<any>()
+
 
   useEffect(() => {
-    setIsModuleLoading(true)
-    _isModuleEnabled()
-    setIsModuleLoading(false)
-  }, [])
+    if (!isModuleEnabled) {
+      _setIsModuleEnabled()
+    }
+    const provider = new ethers.providers.JsonRpcProvider("https://goerli.infura.io/v3/")
+    const moduleContract = new ethers.Contract(bobIsSafeModuleAddress, safeAbi, provider)
+    setModuleContract(moduleContract)
+  }, [isModuleEnabled])
+
+  useEffect(() => {
+    // const filters = contract.filters.LedgerCreated(account)
+    // contract.on(filters, () => {
+    //   alert(`xxx`)
+    // })
+
+    // emit BobModuleSetup();
+    if (moduleContract) {
+      // const filters = moduleContract.filters.BobModuleSetup()
+      // moduleContract.on(filters, () => {
+      //   alert(`Module is enabled`)
+      // })
+    }
+
+  }, [moduleContract])
 
   const enableZKModuleTx = useCallback(async () => {
     try {
@@ -59,7 +80,12 @@ const TransactionForm: React.FC = () => {
         ],
       })
       console.log({ safeTxHash })
+
       const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
+      // sdk.eth.
+      if (safeTx) {
+        setIsModuleEnabled(true)
+      }
       console.log({ safeTx })
     } catch (e) {
       console.error(e)
@@ -87,12 +113,13 @@ const TransactionForm: React.FC = () => {
       console.log({ safeTxHash })
       const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
       console.log({ safeTx })
+      setStatus("txPending")
     } catch (e) {
       console.error(e)
     }
   }, [safe, sdk])
 
-  const _isModuleEnabled = useCallback(async () => {
+  const _setIsModuleEnabled = useCallback(async () => {
     try {
       const ethAdapter = new EthersAdapter({
         ethers,
@@ -111,49 +138,86 @@ const TransactionForm: React.FC = () => {
   }, [safe, sdk])
 
   return (
-    <div>
-      {isModuleEnabled && !isModuleLoading ? (
-        <Form {...layout} form={form} name="control-hooks" onFinish={onFinish} style={{ maxWidth: 600 }}>
-          <Form.Item name="ZkBob" label="zKBob Address" rules={[{ required: true }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="amount" label="Amount" rules={[{ required: true }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="token" label="Token Address" rules={[{ required: true }]}>
-            <Select
-              placeholder="Select a option and change input text above"
-              onChange={() => {
-                alert('ok')
+    status === "txPending" ?
+      <Card title="Wait for the tx to be confirmed" style={{ width: 700 }} extra={<a href="https://google.com">More</a>}>
+        <Spin size="default">
+          <Alert
+            message="Transaction is pending"
+            description="Please wait until the transaction is confirmed."
+            type="info"
+          />
+        </Spin>
+      </Card>
+      : status === "initial" ?
+        <Card title="Send a Direct Deposit using Safe and ZkBob" extra={<a href="https://google.com">More</a>} style={{ width: 700 }}>
+          {isModuleEnabled && status === "initial" ? (
+            <Form {...layout}
+              form={form}
+              name="control-hooks"
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+              style={{
+                maxWidth: 600,
+                border: 10
               }}
-              allowClear
-            >
-              <Option value="0x..">USDC</Option>
-              <Option value="0x..">GHO</Option>
-              <Option value="0x..">OTHER</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item noStyle shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}>
-            {({ getFieldValue }) =>
-              getFieldValue('gender') === 'other' ? (
-                <Form.Item name="customizeGender" label="Customize Gender" rules={[{ required: true }]}>
-                  <Input />
-                </Form.Item>
-              ) : null
-            }
-          </Form.Item>
-          <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit" onClick={submitTx}>
-              Send Direct Deposit
+              labelAlign="right">
+              <Form.Item name="ZkBob Address" label="zKBob Address" rules={[{ required: true, }]}>
+                <Input onChange={(e:any)=> console.log(e.target.value)}/>
+                {/* <Input onChange={(e:any)=> setZkBobAddress(e.target.value)}/> */}
+              </Form.Item>
+              <Form.Item name="Amount" label="Amount" rules={[{ required: true }]}>
+                <Input onChange={(e:any)=> setAmount(e.target.value)}/>
+              </Form.Item>
+              <Form.Item name="Token Address" label="Token Address" rules={[{ required: true }]}
+              >
+                <Select
+                  placeholder="Select a option and change input text above"
+                  onChange={(e:any) => {
+                    setAmount(e)
+                  console.log(e)
+                  }}
+                  allowClear
+                >
+                  <Option value="0x2f3A40A3db8a7e3D09B0adfEfbCe4f6F81927557">USDC</Option>
+                  <Option value="0x..">GHO</Option>
+                  <Option value="0i..">APE</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item noStyle shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}>
+                {({ getFieldValue }) =>
+                  getFieldValue('gender') === 'other' ? (
+                    <Form.Item name="customizeGender" label="Customize Gender" rules={[{ required: true }]}>
+                      <Input />
+                    </Form.Item>
+                  ) : null
+                }
+              </Form.Item>
+              <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit" onClick={submitTx} >
+                  Send Direct Deposit
+                </Button>
+              </Form.Item>
+            </Form>
+          ) : (
+            <Button color="secondary" onClick={enableZKModuleTx}>
+              Click to enable ZK module
             </Button>
-          </Form.Item>
-        </Form>
-      ) : (
-        <Button color="primary" onClick={enableZKModuleTx}>
-          Click to enable ZK module
-        </Button>
-      )}
-    </div>
+          )}
+          {/* </Card> */}
+        </Card> :
+        <Result
+          status="success"
+          title="Successfully Deposited Bob"
+          subTitle="Transaction ID: 123assasa"
+          extra={[
+            <Button type="primary" key="console">
+              Check Tx in Etherscan
+            </Button>,
+            <Button key="restart" onClick={() => {
+              setStatus("initial")
+            }}>Start Again</Button>,
+          ]}
+        />
   )
 }
 
