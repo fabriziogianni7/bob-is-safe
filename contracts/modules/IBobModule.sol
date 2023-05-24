e85189586fa04c5eb89d0f33ebf3f434f39ae222
Frankc <63744530+francescocirulli@users.noreply.github.com>
2023-05-24 16:52:18 +0200

    Update IBobModule.sol

--- contracts/modules/IBobModule.sol
+++ contracts/modules/IBobModule.sol
@@ -3,4 +3,5 @@ pragma solidity >=0.7.0 <0.9.0;
 interface IBobModule {
     event BobModuleSetup(address, address, address, address);
     event DepositSuccess(address, uint256);
+    error PaymentDisabled();
 }
