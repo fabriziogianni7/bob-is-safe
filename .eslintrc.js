faf01c165bb450b5e5546a6df11b8c1ddd57f2a4
Fabriziogianni7 <fabrizio@keyko.io>
2023-05-27 13:11:49 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,10 +18,10 @@ module.exports = {
     'react'
   ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/no-misused-promises': 'off',
-    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/triple-slash-reference': 'off',
     '@typescript-eslint/consistent-type-assertions': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',
